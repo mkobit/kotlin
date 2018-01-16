6e076045ebcb70ae2f9031a5d585a7f1548f9b3a
Sergey Igushkin <hotkeytlt@gmail.com>
2018-01-16 23:40:17 +0300

    Get rid of `gradle-api` artifacts uploaded to and used from JB repo

--- libraries/tools/kotlin-annotation-processing/build.gradle.kts
+++ libraries/tools/kotlin-annotation-processing/build.gradle.kts
@@ -1,6 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.sun.javafx.scene.CameraHelper.project
 import org.gradle.api.internal.artifacts.publish.ArchivePublishArtifact
 import org.gradle.jvm.tasks.Jar
+import javax.xml.ws.Endpoint.publish
 
 description = "Annotation Processor wrapper for Kotlin"
 
@@ -11,8 +13,8 @@ val packedJars by configurations.creating
 dependencies {
     compile(projectDist(":kotlin-stdlib"))
     compileOnly(project(":kotlin-annotation-processing"))
-    compileOnly("org.jetbrains.kotlin:gradle-api:1.6")
-    testCompile("org.jetbrains.kotlin:gradle-api:1.6")
+    compileOnly(gradleApi())
+    testCompile(gradleApi())
     compileOnly("com.android.tools.build:gradle:1.1.0")
     testCompile("com.android.tools.build:gradle:1.1.0")
     testCompile(commonDep("junit:junit"))
